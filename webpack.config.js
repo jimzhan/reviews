9739832815a66a0da8fd8d9558324a781932e4c1
jimzhan <jim.zhan@me.com>
2016-02-24 23:28:34 +0800

    init imports.

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+'use strict';
+let path = require('path');
+let webpack = require('webpack');
+
+
+module.exports = {  
+
+  devServer: {
+    info: false,
+    hot: true,
+    inline: false,
+    port: 8000,
+    host: 'localhost',
+    colors: true,
+    progress: true,
+    contentBase: '.',
+    historyApiFallback: true,
+    stats: {
+      colors: true,
+      progress: true
+    }
+  },
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/static'
+  },
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+}
