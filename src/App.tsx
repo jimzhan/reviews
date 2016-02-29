d2a1b6b4ce306f339e79057c62f195d5f4313901
jimzhan <jim.zhan@me.com>
2016-02-29 22:33:38 +0800

    divide into application components.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+class App extends React.Component<any, any> {
+    public render() {
+        return (
+            <div><h2>Applications</h2></div>
+        );
+    }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-/// <reference path="../typings/tsd.d.ts" />
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import App from './App';
 
 
 document.addEventListener('DOMContentLoaded', function () {
   let main = document.getElementsByTagName('main')[0];
-  ReactDOM.render(<h2>Hello React</h2>, main);
+  ReactDOM.render(<App />, main);
 });
